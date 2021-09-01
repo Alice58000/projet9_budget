887a9484623f9a22854cd253e58cb263b99e1ecc
Alice58000 <finot.alice@gmail.com>
2021-09-01 14:18:20 +0200

    en cours

---
+++ main.js
@@ -0,0 +1,11 @@
+ const budget = document.getElementById("budget");
+ const libelle = document.getElementById("libelle");
+ const montant = document.getElementById("montant");
+ const select = document.getElementById("select");
+ const ajouter = document.getElementById("ajouter");
+ const resultat = document.getElementById("resultat");
+
+montant.addEventListener('click', function() {
+
+    
+})
